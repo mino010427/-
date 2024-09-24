25a245ea3652c86e1abf2bf990780ce905b3b01a
skaxodls <taein5471@gs.cwnu.ac.kr>
2024-09-24 19:20:55 +0900

    테스트

--- taein/master.py
+++ taein/master.py
@@ -39,12 +39,8 @@ class MasterNode:
         # Worker에게 ID를 명시적으로 전송
         client_socket.sendall(worker_id.encode('utf-8'))
 
-        # 4개 Worker Node가 연결될 때까지 대기
-        while True:
-            if self.connected_workers == 4:
-                print("Worker Node 4개 연결, 작업 분배...")
-                self.distribute_config()
-                break
+        print("Worker Node 4개 연결, 작업 분배...")
+   
 
     def distribute_tasks(self):
         while True:
@@ -102,16 +98,10 @@ class MasterNode:
                 with self.lock:  # 큐에 접근할 때 뮤텍스 잠금
                     self.task_queue.put(task_data)
 
-    def distribute_config(self):
-        # 각 Worker Node가 작업을 수신할 수 있도록 스레드를 시작함
-        for worker_socket in self.worker_sockets:
-            threading.Thread(target=self.receive_results, args=(worker_socket,)).start()
-            threading.Thread(target=self.receive_worker_status, args=(worker_socket,)).start()
+   
+            
 
 
-        # 작업 분배를 위한 스레드 시작
-        distribution_thread = threading.Thread(target=self.distribute_tasks)
-        distribution_thread.start()
 
     def find_load_worker(self):
         max_remaining = -1  # 가장 큰 queue_remaining 값을 저장할 변수
@@ -143,7 +133,7 @@ class MasterNode:
 
     def receive_worker_status(self, client_socket, worker_id):
     # Worker Node로부터 주기적으로 큐 상태를 수신
-        while True:
+        
             try:
                 status_data = client_socket.recv(1024).decode()  # Worker Node로부터 상태 수신
                 if status_data:
@@ -156,7 +146,7 @@ class MasterNode:
                     print(f"Worker {worker_id} 상태 - 사용 중: {status['queue_used']}, 남은 공간: {status['queue_remaining']}")
             except Exception as e:
                 print(f"Worker {worker_id} 상태 수신 오류: {e}")
-                break
+                
    
     def receive_results(self, worker_socket):
         # 각 Worker Node로부터 결과 수신 및 재할당 처리
@@ -177,6 +167,8 @@ class MasterNode:
                         task_data = result.split("C[")[1].split(']')[0]  # C[i, j]에서 i, j만 추출
                         i, j = task_data.split(', ')
                         print(f"작업성공: {self.worker_ids[worker_socket]} / C[{i}, {j}]")
+
+                        
                 time.sleep(1)  # 통신 지연 시뮬레이션
         except Exception as e:
             print(f"오류!: {self.worker_ids[worker_socket]} / {e}")
@@ -193,12 +185,22 @@ class MasterNode:
         # Worker Node의 접속을 기다림
         while self.connected_workers < 4:
             client_socket, address = server_socket.accept()
-            worker_thread = threading.Thread(target=self.handle_worker, args=(client_socket, address))
-            worker_thread.start()
+            self.handle_worker(client_socket, address)
+            
+            #작업 결과를 받기 위한 스레드 시작
+            threading.Thread(target=self.receive_results, args=(client_socket)).start()
+            
+
 
         # 작업 추가를 위한 스레드 시작
         task_addition_thread = threading.Thread(target=self.add_tasks_to_queue)
         task_addition_thread.start()
+
+ 
+
+        # 작업 분배를 위한 스레드 시작
+        distribution_thread = threading.Thread(target=self.distribute_tasks)
+        distribution_thread.start()
 
 # Google Cloud VM에서 실행될 Master Node
 if __name__ == "__main__":
